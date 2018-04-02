d9a65058f5ea1ad155aaf9c603ae1f8e483c0c6c
Jacksadf <kotzecaasdfbo@live.comasdf>
2018-04-02 21:07:56 +0200

    Initial Commit

---
+++ MergeMediaSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MergeMediaSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
