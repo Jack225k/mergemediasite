1e5b3ba9dfecbe0b85fb69b96c5b643e1cf2df5d
kaybo <kotzecabo@live.com>
2018-08-23 14:18:50 +0200

    New Index file and layout

---
+++ Mergemediawebsite/Controllers/PortfolioController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mergemediawebsite.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Demo1")]
+        public IActionResult Demo1()
+        {
+            ViewData["Message"] = "About Page";
+
+            return Redirect("~/lib/demo/aligned/index.html");
+        }
+    }
+}
